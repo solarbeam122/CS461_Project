398d949d35b54d0a807fba6e2a9d967a5ee37887
solarbeam122 <atticusrum@gmail.com>
2023-03-28 16:17:42 -0600

    Update simpleshell.c

--- simpleshell.c
+++ simpleshell.c
@@ -4,65 +4,114 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-
-/*
-Justus Contreras
-This program is a basic implementation of a 
-shell in the C programming language.*/
+#include <fcntl.h>
+#include <sys/errno.h>
 
 #define MAX_LINE 80 /* The maximum length command */
 
-int main(){
+// This function takes a character buffer and parses it into tokens using strtok
+// It returns the number of arguments read
+int parse(char *buff, char *argv[]) {
+    int j = -1;
+    char *point;
+    point = strtok(buff, " ");
+    while (point != NULL) {
+        j++;
+        argv[j] = point;
+        point = strtok(NULL, " ");
+    }
+    argv[j + 1] = '\0';
+    return (j);
+}
 
-    while(1){
-        // Read the user input
+int main() {
+    // This is the main loop that will run until the user exits the shell
+    while (1) {
+        // Read the user's command and store it in the 'command' variable
         char command[MAX_LINE];
-        // Get the current working directory
         char cwd[1024];
         getcwd(cwd, sizeof(cwd));
-        // Print the prompt with the current directory
         printf("%s > ", cwd);
-        // Read the command from stdin
+        
         fgets(command, MAX_LINE, stdin);
-        // Remove the trailing newline character from the command
-        command[strlen(command)-1] = '\0';
-        // Split the command into arguments
-        char *args[MAX_LINE/2 + 1];
-        int i = 0;
-        char *token = strtok(command, " ");
-        while(token != NULL){
-            args[i] = token;
-            token = strtok(NULL, " ");
-            i++;
-        }
-        args[i] = NULL;
-        // Check if the command is "cd"
-        if(strcmp(args[0], "cd") == 0){
-            // Change the current working directory
-            if(args[1] == NULL){
-                // If no argument is given, change to the home directory
+        command[strlen(command) - 1] = '\0';
+
+        // Parse the user's command and store the arguments in the 'args' array
+        char *args[MAX_LINE / 2 + 1];
+        int numArgs = parse(command, args);
+
+        // Check if the command is 'cd' (change directory)
+        if (strcmp(args[0], "cd") == 0) {
+            // If there's no directory specified, change to the home directory
+            if (args[1] == NULL) {
                 chdir(getenv("HOME"));
-            }
-            else{
-                // Change to the given directory
+            } else {
+                // Change to the specified directory
                 chdir(args[1]);
             }
-        }
-        // Otherwise, execute the command using a child process
-        else{
+        } else {
+            // This is the main process that will spawn a child process to execute the command
+            printf("I'm in the main process.\n");
+
+            // Create a child process using fork
             pid_t pid = fork();
-            if(pid == 0){
-                // Child process: execute the command
-                execvp(args[0], args);
-                // If execvp returns, there was an error
-                printf("Error executing command\n");
-                exit(0);
+
+            // If pid > 0, this is the parent process
+            if (pid > 0) {
+                printf("I'm the parent, waiting for the child process.\n");
+                // The parent waits for the child process to complete its execution
+                waitpid(pid, NULL, 0);
+                printf("Parent is done waiting.\n");
             }
-            else{
-                // Parent process: wait for the child to finish
-                wait(NULL);
+            // If pid == 0, this is the child process
+            else if (pid == 0) {
+                printf("I'm the child, executing the command.\n");
+
+                int input = -1, output = -1;
+
+                // Loop through the arguments to look for input/output redirection symbols
+                for (int i = 0; i < numArgs; i++) {
+                    // If the argument is '<', set up input redirection
+                    if (strcmp(args[i], "<") == 0) {
+                        args[i] = NULL;
+                        // Open the input file
+                        input = open(args[i + 1], O_RDONLY);
+                        if (input < 0) {
+                            perror("Error opening input file");
+                            exit(1);
+                        }
+                        // Duplicate the file descriptor for input to STDIN_FILENO
+                        dup2(input, STDIN_FILENO);
+                        // Close the original file descriptor
+                        close(input);
+                    }
+                    // If the argument is '>', set up output redirection
+                    else if (strcmp(args[i], ">") == 0) {
+                        args[i] = NULL;
+                        // Open the output file
+                        output = open(args[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                        // If the output file cannot be opened, print an error message and exit
+                        if (output < 0) {
+                            perror("Error opening output file");
+                            exit(1);
+                        }
+                        // Duplicate the file descriptor for output to STDOUT_FILENO
+                        dup2(output, STDOUT_FILENO);
+                        close(output);
+                    }
+                }
+                // Execute the command
+                execvp(args[0], args);
+                // If the execvp function returns, it means that the command was not found
+                perror("Error executing command");
+                exit(1);
+                // If pid < 0, there was an error creating the child process
+            } else {
+                perror("Error creating child process");
+                exit(1);
             }
         }
     }
+    // Exit the shell
     return 0;
 }
